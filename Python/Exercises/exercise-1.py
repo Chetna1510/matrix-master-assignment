26ddf95367ce38ee0ae4694dedd766ca99481b5c
Chetna1510 <chetna.bhutka@outlook.com>
2025-11-20 22:24:38 +0100

    Initial commit: Add Materialize project

---
+++ Python/Exercises/exercise-1.py
@@ -0,0 +1,25 @@
+# 1. Create a variable age with your age and print it.
+
+import math
+age = 31
+print(age)
+
+# 2. Store your name in a string variable and print "Hello, <your name>".
+
+name = "Chetna"
+print(name)
+
+# 3. Make a list of 5 favorite fruits and print the second fruit.
+
+fruitList = ["Grapes", "Apple", "Mango", "Orange", "Musk melon"]
+print(fruitList[1])
+
+# 4. Convert the string "123" into an integer and add 10 to it.
+
+num = int("123")   # Convert string to integer
+result = num + 10  # Add 10
+print(result)
+
+# 5. Import the math module and print the square root of 16.
+
+print(math.sqrt(16))
